8595b50ecba4335aba14b36a8efc5abf1a9f2942
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-17 20:13:25 +0800

    [STToDoList][Robert] docs: Added React files after creating react app

---
+++ react-todolist/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ToDoList from './features/todos/components/ToDoList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoList/>
+    </div>
+  );
+}
+
+export default App;
