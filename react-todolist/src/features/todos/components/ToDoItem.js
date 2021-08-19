aa81bdece84dc436584febe0216a06aadd13ad92
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-19 20:06:55 +0800

    [STToDoList][Robert] feat: Added edit button with modal that allows editing of current todo text

--- react-todolist/src/features/todos/components/ToDoItem.js
+++ react-todolist/src/features/todos/components/ToDoItem.js
@@ -1,15 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectToDoById, RemoveToDoFromState,ToggleToDoFromState } from '../reducers/todoSlice';
 import { updateToDo, deleteToDo } from '../../apis/todos'
-import { Button } from 'antd';
+import { Input, Modal, Button } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
 
 
 function ToDoItem(props) {
-    
+
     const dispatch = useDispatch();
     const todo = useSelector((state) => selectToDoById(state, props.toDoId))
+    const [modalText, setModalText] = useState(todo.text)
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setModalText(todo.text)
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+
+
     var todoItemClass = todo.done? 'todo-item-done' : 'todo-item';
+    var editButtonClass = todo.done? 'edit-button-done' : 'edit-button';
 
     function RemoveToDo(e) {
         deleteToDo(todo.id)
@@ -20,15 +36,36 @@ function ToDoItem(props) {
     }
 
     function ToggleToDo() {
-        updateToDo(todo.id, !todo.done)
+        updateToDo(todo.id, {done: !todo.done})
         .then((response) => {
             dispatch(ToggleToDoFromState(response.data))
         })
     }
 
+    function ModifyToDo() {
+        if (modalText !== '') {
+            updateToDo(todo.id, {text: modalText})
+            .then((response) => {
+                dispatch(ToggleToDoFromState(response.data))
+            })
+            setIsModalVisible(false);
+        } else {
+            handleCancel()
+        }
+
+    }
+
+    function HandleModify(e) {
+        setModalText(e.target.value)
+    } 
+
     return (
-        <div class="todo-div" onClick={ToggleToDo}>
-            <span className={todoItemClass}>{todo.text}</span><Button type="danger" onClick={RemoveToDo}>X</Button>
+        <div className="todo-div">
+            <span className={todoItemClass} onClick={ToggleToDo}>{todo.text}</span><Button type="danger" onClick={RemoveToDo}>X</Button>
+            <Button className={editButtonClass} icon={<EditOutlined />} type="primary" onClick={showModal}></Button>
+      <Modal title="Edit to-do" visible={isModalVisible} onOk={ModifyToDo} onCancel={handleCancel}>
+        <Input value={modalText} onChange={HandleModify}></Input>
+      </Modal>
         </div>
     );
 }
